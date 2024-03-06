94a24d2f1630474a1fc4a9e260c10884551728c5
MaysaMosa <158842487+MaysaMosa@users.noreply.github.com>
2024-03-06 16:50:08 +0200

    BssB2423-75
    login page for worker

--- MYTOWNWEB/mytown/models.py
+++ MYTOWNWEB/mytown/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class AddReport(models.Model):
+    title = models.CharField(max_length=500)
+    email = models.EmailField()
+
+    CITY_CHOICES = [
+        ('BEER', 'Beer Sheva'),
+        ('ASD', 'Ashdod'),
+        ('ASHQ', 'Ashkelon'),
+        ('NEOT', 'Netivot'),
+        ('OFK', 'Ofakim'),
+        ('SDR', 'Sderot'),
+        ('KRM', 'Kiryat Malachi'),
+        ('KSP', 'Kiryat Gat'),
+        ('NPM', 'Netiv HaAsara'),
+        ('REH','Rehovot'),  
+        ('OME', 'Omer'),
+        ('MET', 'Metar'),  
+        ('RAH', 'Rahat'),  
+        ('LAk', 'Lakiya'),  
+        ('HURA', 'Hura'),  
+        ('HOL', 'Holon'),
+        ('KSI','Ksifa'),
+        ('DEM','Dimona'),
+         ('ARAD', 'Arad'),  
+    ]
+
+    def __str__(self):
+        return self.title
+
+    city = models.CharField(max_length=5, choices=CITY_CHOICES)
+    description=models.CharField(max_length=1000)
+    location=models.CharField(max_length=500)
+    picture = models.ImageField(upload_to='report_pictures/', null=True, blank=True)
+
+
+
+
+class Workerlogin(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username

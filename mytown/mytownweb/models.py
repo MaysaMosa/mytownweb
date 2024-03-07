b9021af1c1468667d8cb87c419ae91a58f441665
IbtihalAlnbary <159442598+IbtihalAlnbary@users.noreply.github.com>
2024-03-07 23:50:04 +0200

    sugnup

---
+++ mytown/mytownweb/models.py
@@ -0,0 +1,20 @@
+# from django.db import models
+
+# # Create your models here.
+# from django.db import models
+
+
+# class signup(models.Model):
+#     username = models.CharField(max_length=100, unique=True)
+#     fname = models.CharField(max_length=255)
+#     lname = models.CharField(max_length=500)
+#     email = models.EmailField()
+#     pass1 = models.CharField(max_length=255)
+#     pass2 = models.CharField(max_length=255)
+
+#     class Meta:
+#         db_table = 'signup'
+#     def __str__(self):
+#         return self.username
+
+
---
+++ mytown/mytownweb/urls.py
@@ -0,0 +1,29 @@
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from . import views 
+
+# #list of all urls i want add
+# urlpatterns = [
+#     path("", views.home,name="home")
+# ]
+
+
+
+
+from django.urls import path
+from . import views 
+
+#list of all urls i want add
+# urlpatterns = [
+#  path("citizen/",views.index)
+# ]
+
+urlpatterns = [
+   
+ path('',views.home,name="home"),
+ path('signup/',views.signup,name="signup"),
+ path('signin/',views.signin,name="signin"),
+ path('signout/',views.signout,name="signout"),
+
+]
